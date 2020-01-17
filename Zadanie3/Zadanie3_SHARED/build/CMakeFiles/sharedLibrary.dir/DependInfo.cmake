2c170a351c24f23410d76294f30da5aabd70626a
mateuszkro <44154566+mateuszkro@users.noreply.github.com>
2020-01-17 00:17:14 -0800

    Add files via upload

---
+++ Zadanie3/Zadanie3_SHARED/build/CMakeFiles/sharedLibrary.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kicek/Desktop/Kolokwium/Zadanie3/Zadanie3_SHARED/src/Student.cpp" "/home/kicek/Desktop/Kolokwium/Zadanie3/Zadanie3_SHARED/build/CMakeFiles/sharedLibrary.dir/src/Student.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "sharedLibrary_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Zadanie3/Zadanie3_SHARED/build/CMakeFiles/sharedLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sharedLibrary.dir/src/Student.cpp.o"
+  "libsharedLibrary.pdb"
+  "libsharedLibrary.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sharedLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
