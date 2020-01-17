7f4c12db07a94375619eb20e534af2191cb14f37
mateuszkro <44154566+mateuszkro@users.noreply.github.com>
2020-01-17 00:19:33 -0800

    Add files via upload

---
+++ Zadanie3/Zadanie3_USE_LIB/build/CMakeFiles/myProgram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myProgram.dir/libtest.cpp.o"
+  "myProgram"
+  "myProgram.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
